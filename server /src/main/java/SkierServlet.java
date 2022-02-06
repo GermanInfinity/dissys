f6af28821c0d60b2ede3d24840631f9c49d3c6f3
Akwarandu Ugo Nwachuku <30478514+GermanInfinity@users.noreply.github.com>
2022-02-06 10:35:22 -0800

    Updated threads work
    Working on lab3 + Updated threads work

---
+++ server /src/main/java/SkierServlet.java
@@ -0,0 +1,85 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
+@WebServlet(name = "SkierServlet", value = "/SkierServlet")
+public class SkierServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        res.setContentType("text/plain");
+        String urlPath = req.getPathInfo();
+
+        // check we have a URL!
+        if (urlPath == null || urlPath.isEmpty()) {
+            res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            res.getWriter().write("missing parameters");
+            return;
+        }
+        String[] urlParts = urlPath.split("/");
+
+        if (!isUrlValid(urlParts)) {
+            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } else {
+            res.setStatus(HttpServletResponse.SC_OK);
+            res.getWriter().write("It works now!");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String urlPath = req.getPathInfo();
+
+        // check we have a URL!
+        if (urlPath == null || urlPath.isEmpty()) {
+            res.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            res.getWriter().write("missing parameters");
+            return;
+        }
+        String[] urlParts = urlPath.split("/");
+
+        if (!isUrlValid(urlParts)) {
+            res.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        } else {
+            BufferedReader req_body = req.getReader();
+            StringBuilder body = new StringBuilder();
+            String line;
+            while( (line = req_body.readLine()) != null) {
+                body.append(line);
+            }
+            System.out.println(body);
+            res.getWriter().write("Post sent!");
+        }
+    }
+    private boolean isUrlValid(String[] urlPath) {
+        // Validation: Length of UrlPaths, presence of integer and alphabet
+        // at desired location
+        if (urlPath.length != 8) { return false; };
+        //if (urlPath[0] == '') { return false; };
+        if (isNumeric(urlPath[1]) == false) { return false; };
+        if (isNumeric(urlPath[3]) == false) { return false; };
+        if (isNumeric(urlPath[5]) == false) { return false; };
+        if (isNumeric(urlPath[7]) == false) { return false; };
+        if (isAlpha(urlPath[2]) == false) { return false; };
+        if (isAlpha(urlPath[4]) == false) { return false; };
+        if (isAlpha(urlPath[6]) == false) { return false; };
+        return true;
+    }
+
+    // Function determines if a string is all integers.
+    public static boolean isNumeric(String str) {
+        try {
+            Double.parseDouble(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+
+    // Function determines if a string is all alphabets.
+    public static boolean isAlpha(String s) {
+        return s != null && s.chars().allMatch(Character::isLetter);
+    }
+}
--- threads/src/threads/thread.java
+++ threads/src/threads/thread.java
@@ -1,5 +1,6 @@
 package threads;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class thread {
     final static private int THREADS_NUM = 10000;
@@ -14,7 +15,6 @@ public class thread {
 		for (int i = 0; i < 1000; i++) { 
 			vec.add("thread" + i);
 		}
-		System.out.println(vec);
     }
 
     synchronized public void add_to_arraylist() { 
@@ -22,7 +22,27 @@ public class thread {
 		for (int i = 0; i < 1000; i++) { 
 			arraylist.add("thread" + i);
 		}
-		System.out.println(arraylist);
+    }
+    
+    synchronized public void add_to_hashtable() { 
+    	Hashtable<Integer, String> ht1 = new Hashtable<>();
+		for (int i = 0; i < 1000; i++) { 
+			ht1.put(i, "added");
+		}
+    }
+    
+    synchronized public void add_to_hashmap() { 
+    	HashMap<String, String> maps = new HashMap<String, String>();
+		for (int i = 0; i < 1000; i++) { 
+		    maps.put("This", "added");
+		}
+    }
+    
+    synchronized public void add_to_concurrenthashmap() { 
+    	ConcurrentHashMap<Integer, String> m = new ConcurrentHashMap<>();
+		for (int i = 0; i < 1000; i++) { 
+		    m.put(i, "added");
+		}
     }
     
     public int get_value() { 
@@ -47,16 +67,17 @@ public class thread {
 
 	
 	public static void collections_vector() throws InterruptedException { 
-		final thread counter = new thread();
+	    final thread counter = new thread();
 		
 		long time1 = System.nanoTime();
 		
 		Runnable t = () -> { counter.add_to_vector(); };
-		new Thread(t).start();
-		
+		Thread var =  new Thread(t);
+		var.start();
+		var.join();
 		long time2 = System.nanoTime();
-		long timeTaken = time2 - time1;  
-		Thread.sleep(500);
+		long timeTaken = time2 - time1; 
+		
 		System.out.println("Adding to vector on single thread takes:" + timeTaken + " ns");  
 		
 	}
@@ -67,26 +88,31 @@ public class thread {
 		long time1 = System.nanoTime();
 		
 		Runnable t = () -> { counter.add_to_arraylist(); };
-		new Thread(t).start();
-		
+		Thread var = new Thread(t);
+		var.start();
+		var.join();
 		long time2 = System.nanoTime();
 		long timeTaken = time2 - time1;  
-		Thread.sleep(500);
+		
 		System.out.println("Adding to arraylist on single thread takes: " + timeTaken + " ns");  
 		
 	}
 	
 	public static void collections_hashtable() throws InterruptedException { 
+		// create multiple threads with a for loop, and each loop each thread
+		// runs the runnable 
+		// time the whole for loop
 		final thread counter = new thread();
 		
 		long time1 = System.nanoTime();
 		
-		Runnable t = () -> { counter.add_to_arraylist(); };
-		new Thread(t).start();
-		
+		Runnable t = () -> { counter.add_to_hashtable(); };
+		Thread var = new Thread(t);
+		var.start();
+		var.join();
 		long time2 = System.nanoTime();
 		long timeTaken = time2 - time1;  
-		Thread.sleep(500);
+
 		System.out.println("Adding to hashtable on single thread takes: " + timeTaken + " ns");  
 		
 	}
@@ -96,27 +122,29 @@ public class thread {
 		
 		long time1 = System.nanoTime();
 		
-		Runnable t = () -> { counter.add_to_arraylist(); };
-		new Thread(t).start();
-		
+		Runnable t = () -> { counter.add_to_hashmap(); };
+		Thread var = new Thread(t);
+		var.start();
+		var.join();
 		long time2 = System.nanoTime();
 		long timeTaken = time2 - time1;  
-		Thread.sleep(500);
+
 		System.out.println("Adding to hashmap on single thread takes: " + timeTaken + " ns");  
 		
 	}
 	
-	public static void collections_chashmap() throws InterruptedException { 
+	public static void collections_concurrent_hashmap() throws InterruptedException { 
 		final thread counter = new thread();
 		
 		long time1 = System.nanoTime();
 		
-		Runnable t = () -> { counter.add_to_arraylist(); };
-		new Thread(t).start();
-		
+		Runnable t = () -> { counter.add_to_concurrenthashmap(); };
+		Thread var = new Thread(t);
+		var.start();
+		var.join();
 		long time2 = System.nanoTime();
 		long timeTaken = time2 - time1;  
-		Thread.sleep(500);
+
 		System.out.println("Adding to concurrent hashmap on single thread takes: " + timeTaken + " ns");  
 		
 	}
@@ -133,6 +161,9 @@ public class thread {
 		// Step 2: Collections
 		collections_vector();
 		collections_arraylist();
+		collections_hashtable();
+		collections_hashmap();
+		collections_concurrent_hashmap();
 		
 //		String test = "Hello Ugo how-are you";
 //		String[] out = test.split("-");
